9d57a1fa96b73e4b3f1355b695d4dd61e4119f1a
Rails Udemy Course <gochualex@gmail.com>
2016-03-22 00:49:54 +0000

    Add assocoation between user and articles and add user view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,15 +14,18 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    #render plain: article_params
-   @article = Article.new(article_params)
     
-   if @article.save
-     flash[:success] = "Article was successfully created."
-     redirect_to article_path(@article)
-   else
-     render :new
-   end
+    #render plain: article_params
+    debugger
+    @article = Article.new(article_params)
+    @article.user = User.first
+    
+    if @article.save
+      flash[:success] = "Article was successfully created."
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
   end
   
   def update 
