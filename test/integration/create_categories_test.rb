309ddb0ba6deb3210338731b8f2116f3eec46ea8
Rails Udemy Course <gochualex@gmail.com>
2016-03-30 20:36:27 +0000

    Fix tests and update navbar with categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,8 +2,13 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @user = User.create(username:"john", email: "john@john.com", password: "password", admin: true)
+  end
+  
   test "get new category form and create category" do
     
+    sign_in_as(@user, "password")
     get new_category_path
     
     assert_template 'categories/new'
@@ -19,6 +24,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   
   test "invalid category submission results in failure" do
     
+    sign_in_as(@user, "password")
     get new_category_path
     
     assert_template 'categories/new'
